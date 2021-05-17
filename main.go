a87ede6a02a2f3d01d4187b21083314a868bb0b3
onozaty <onozaty@gmail.com>
2021-05-18 00:05:16 +0900

    指定フォルダ内のテキストファイルを検索するツール作成

---
+++ main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	flag "github.com/spf13/pflag"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+)
+
+func main() {
+
+	if len(commit) > 7 {
+		commit = commit[:7]
+	}
+
+	var help bool
+	var inputDir string
+	var outputDir string
+	var caseSensitive bool
+
+	flag.StringVarP(&inputDir, "input", "i", "", "Input directory. Specify the directory where the text files to be searched are located.")
+	flag.StringVarP(&outputDir, "output", "o", "", "Output directory. The text files that match the search will be placed.")
+	flag.BoolVarP(&caseSensitive, "cs", "c", false, "The search is case-sensitive. By default, it is not case-sensitive.")
+	flag.BoolVarP(&help, "help", "h", false, "Help.")
+	flag.Parse()
+	flag.CommandLine.SortFlags = false
+	flag.Usage = func() {
+		fmt.Printf("txtsearch v%s (%s)\n", version, commit)
+		fmt.Fprintf(os.Stderr, "Usage: %s [flags] WORDS...\nflags\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	if help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	words := flag.Args()
+
+	if len(words) == 0 || inputDir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	result, err := search(inputDir, words, caseSensitive)
+
+	if err != nil {
+		fmt.Println("\nError: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("total: %d, match: %d\n", result.totalCount, len(result.matchFiles))
+
+	if outputDir != "" {
+		// 出力ディレクトリが指定されていた場合
+		// -> 一致したファイルを出力
+
+		_, err := os.Stat(outputDir)
+		if os.IsNotExist(err) {
+			os.Mkdir(outputDir, 0777)
+		} else {
+			fmt.Println("\nError: ", err)
+			os.Exit(1)
+		}
+
+		err = copyFiles(result.matchFiles, outputDir)
+		if err != nil {
+			fmt.Println("\nError: ", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("copy the matched files to %s\n", outputDir)
+	} else {
+		for _, matchFile := range result.matchFiles {
+			fmt.Println(matchFile)
+		}
+	}
+
+	if err != nil {
+		fmt.Println("\nError: ", err)
+		os.Exit(1)
+	}
+}
+
+type SearchResult struct {
+	totalCount int
+	matchFiles []string
+}
+
+func search(inputDir string, words []string, caseSensitive bool) (SearchResult, error) {
+
+	filePaths, err := getFiles(inputDir)
+	if err != nil {
+		return SearchResult{}, err
+	}
+
+	matchFiles := []string{}
+	for _, filePath := range filePaths {
+
+		match, err := match(filePath, words, caseSensitive)
+		if err != nil {
+			return SearchResult{}, err
+		}
+		if match {
+			matchFiles = append(matchFiles, filePath)
+		}
+	}
+
+	return SearchResult{
+		totalCount: len(filePaths),
+		matchFiles: matchFiles,
+	}, nil
+}
+
+func match(path string, words []string, caseSensitive bool) (bool, error) {
+
+	if !caseSensitive {
+		for i, word := range words {
+			words[i] = strings.ToLower(word)
+		}
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+
+		line := scanner.Text()
+		if !caseSensitive {
+			line = strings.ToLower(line)
+		}
+
+		for _, word := range words {
+			if strings.Contains(line, word) {
+				return true, nil
+			}
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return false, err
+	}
+
+	return false, nil
+}
+
+func getFiles(baseDir string) ([]string, error) {
+
+	fileInfos, err := ioutil.ReadDir(baseDir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filePaths []string
+	for _, fileInfo := range fileInfos {
+		if !fileInfo.IsDir() {
+			filePaths = append(filePaths, filepath.Join(baseDir, fileInfo.Name()))
+		}
+	}
+
+	sort.Strings(filePaths)
+	return filePaths, nil
+}
+
+func copyFiles(srcFiles []string, outputDir string) error {
+
+	for _, srcFile := range srcFiles {
+		err := copyFile(srcFile, filepath.Join(outputDir, filepath.Base(srcFile)))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copyFile(src string, dest string) error {
+
+	sf, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+
+	df, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+
+	_, err = io.Copy(sf, df)
+	return err
+}
---
+++ main_test.go
@@ -0,0 +1,248 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestConvert_match_match(t *testing.T) {
+
+	s := "word1"
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"word1"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if !result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_match_unmatch(t *testing.T) {
+
+	s := "word1"
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"word2"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_match_match_multiline(t *testing.T) {
+
+	s := `word1
+word2 word3
+word4
+`
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"word4"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if !result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_match_match_multiword(t *testing.T) {
+
+	s := `word1
+word2 word3
+word4
+`
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"wordx", "word2"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if !result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_match_match_casenotsensitive(t *testing.T) {
+
+	s := `word1
+Word2 word3
+word4
+`
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"worD2"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if !result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_match_match_casesensitive(t *testing.T) {
+
+	s := `word1
+Word2 word3
+word4
+`
+
+	f, err := createTempFile(s)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.Remove(f.Name())
+
+	result, err := match(f.Name(), []string{"worD2"}, true)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if result {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_search(t *testing.T) {
+
+	dir, err := os.MkdirTemp("", "test")
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.RemoveAll(dir)
+
+	file1 := filepath.Join(dir, "1")
+	err = os.WriteFile(file1, []byte("word1"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	file2 := filepath.Join(dir, "2")
+	err = os.WriteFile(file2, []byte("word2"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	file3 := filepath.Join(dir, "3")
+	err = os.WriteFile(file3, []byte("word3"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result, err := search(dir, []string{"word1", "WORD3"}, false)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	expect := SearchResult{
+		totalCount: 3,
+		matchFiles: []string{
+			filepath.Join(dir, "1"),
+			filepath.Join(dir, "3"),
+		},
+	}
+
+	if !reflect.DeepEqual(result, expect) {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestConvert_copyFiles(t *testing.T) {
+
+	inDir, err := os.MkdirTemp("", "input")
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.RemoveAll(inDir)
+
+	file1 := filepath.Join(inDir, "1")
+	err = os.WriteFile(file1, []byte("word1"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	file2 := filepath.Join(inDir, "2")
+	err = os.WriteFile(file2, []byte("word2"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	file3 := filepath.Join(inDir, "3")
+	err = os.WriteFile(file3, []byte("word3"), 0666)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	outDir, err := os.MkdirTemp("", "ouput")
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	defer os.RemoveAll(outDir)
+
+	err = copyFiles([]string{file2, file3}, outDir)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	// コピー先のフォルダ確認
+	fileInfos, err := ioutil.ReadDir(outDir)
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if len(fileInfos) != 2 {
+		t.Fatal("failed test\n", fileInfos)
+	}
+}
+
+func createTempFile(content string) (*os.File, error) {
+
+	tempFile, err := os.CreateTemp("", "txt")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = tempFile.Write([]byte(content))
+	if err != nil {
+		return nil, err
+	}
+
+	return tempFile, nil
+}
